b3402a11456b4621ef900b21f0667e7c05538c94
Fei Rune <106476973+ShinosakiTanaka@users.noreply.github.com>
2025-11-22 22:45:45 +0700

    Update lab1_9.java

--- lab1_9.java
+++ lab1_9.java
@@ -1,18 +1,16 @@
 package lab1_9;
-
 import java.util.Scanner;
-
 public class lab1_9 {
-    public static void main(String[] args) {
-        Scanner kb = new Scanner(System.in);
-        int n = kb.nextInt(); 
-        double sum = 0;
-        for(int i = 0; i < n; i++){
-            double x = kb.nextDouble();
-            sum = sum + x; 
-        }
-        double avg = sum / n;
-        System.out.println(avg);
-        kb.close();
-    }
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int n = input.nextInt();
+        double total = 0;
+        for (int i = 0; i < n; i++) {
+            double number = input.nextDouble();
+            total += number;
+        }
+        double average = total / n;
+        System.out.println(average);
+        input.close();
+    }
 }
