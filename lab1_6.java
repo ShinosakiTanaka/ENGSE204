4b89c8d4b6b517c8fb795910e635dca6ee8ce162
Fei Rune <106476973+ShinosakiTanaka@users.noreply.github.com>
2025-11-23 14:31:18 +0700

    Update lab1_6.java

--- lab1_6.java
+++ lab1_6.java
@@ -1,18 +1,14 @@
 package lab1_6;
 import java.util.Scanner;
-
 public class lab1_6 {
     public static void main(String[] args) {
-
         Scanner sc = new Scanner(System.in);
-
         int n = sc.nextInt();
-
         for (int i = 1; i <= 12; i++) {
             int result = n * i;
             System.out.println(n + " x " + i + " = " + result);
         }
-
         sc.close();
     }
 }
+
