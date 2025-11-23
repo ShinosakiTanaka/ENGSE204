b986516c187b482244079a316b75d8b5d95c9b29
Fei Rune <106476973+ShinosakiTanaka@users.noreply.github.com>
2025-11-23 19:27:14 +0700

    Update lab1_7.java

--- lab1_7.java
+++ lab1_7.java
@@ -1,10 +1,17 @@
 package lab1_7;
-
+import java.util.Scanner;
 public class lab1_7 {
+ public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+    int sum = 0;
+    for (int i = 0; i < n; i++) {
+        int value = sc.nextInt();
+        sum = sum + value;
+    }
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+    System.out.println(sum);
 
-	}
-
+    sc.close();
+ }
 }
