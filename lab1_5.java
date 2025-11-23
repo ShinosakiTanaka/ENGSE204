40728767a1e704376d168969b4f851c3ac0fba5a
Fei Rune <106476973+ShinosakiTanaka@users.noreply.github.com>
2025-11-23 18:53:56 +0700

    Update lab1_5.java

--- lab1_5.java
+++ lab1_5.java
@@ -1,10 +1,18 @@
-package lab1_5;
-
+import java.util.Scanner;
 public class lab1_5 {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int m = sc.nextInt();
+        if (m == 1)
+            System.out.println("Americano");
+        else if (m == 2)
+            System.out.println("Latte");
+        else if (m == 3)
+            System.out.println("Espresso");
+        else if (m == 4)
+            System.out.println("Mocha");
+        else
+            System.out.println("Invalid Menu");
+        sc.close();
+ }
 }
