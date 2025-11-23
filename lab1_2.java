3b31c1887ee904d43b590995be129af057991403
Fei Rune <106476973+ShinosakiTanaka@users.noreply.github.com>
2025-11-23 22:57:19 +0700

    Update lab1_2.java

--- lab1_2.java
+++ lab1_2.java
@@ -1,17 +1,15 @@
 package lab1;
-
 import java.util.Scanner;
-
 public class lab1_2 {
+   public static void main(String[] args) {
+      Scanner input = new Scanner(System.in);
+      
+      double width = input.nextDouble();
+      double height = input.nextDouble();
 
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
+      double rectangleArea = width * height;
 
-        double width  = input.nextDouble();
-        double height = input.nextDouble();
-        double rectangleArea = width * height;
-
-        System.out.println(rectangleArea);
-        input.close();
-    }
+     System.out.println(rectangleArea);
+     input.close();
+  }
 }
