ae4c7d3efbd98bb29a013c3949b862a09d46b847
Fei Rune <106476973+ShinosakiTanaka@users.noreply.github.com>
2025-11-29 11:21:59 +0700

    Create lab2_2.java

---
+++ lab2_2.java
@@ -0,0 +1,30 @@
+package lab2;
+
+import java.util.Scanner;
+
+class Student {
+
+    String studentId;
+    String name;
+
+    void displayInfo() {
+        System.out.println(studentId);
+        System.out.println(name);
+    }
+}
+
+public class Lab2_2 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        Student std = new Student();
+
+        std.studentId = sc.nextLine();
+        std.name = sc.nextLine();
+
+        std.displayInfo();
+
+        sc.close();
+    }
+}
