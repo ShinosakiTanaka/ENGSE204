6e01adecd2c7adf965bb61258dbf54596924bf3b
Fei Rune <106476973+ShinosakiTanaka@users.noreply.github.com>
2025-12-12 16:30:57 +0700

    Update lab3.1.java

--- Lab3/lab3.1.java
+++ Lab3/lab3.1.java
@@ -1,1 +1,25 @@
+import java.util.Scanner;
 
+class User {
+    private String USERNAME;
+
+    User(String USERNAME) {
+        this.USERNAME = USERNAME;
+    }
+
+    public String getUsername() {
+        return USERNAME;
+    }
+}
+
+public class lab3_1 {
+    public static void main(String[] args) {
+        Scanner SC = new Scanner(System.in);
+
+        String USERNAME = SC.nextLine();
+
+        User USER = new User(USERNAME);
+
+        System.out.println(USER.getUsername());
+    }
+}
